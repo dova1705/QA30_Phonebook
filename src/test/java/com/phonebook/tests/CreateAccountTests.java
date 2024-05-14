b6a186526f02e153c7b2346d25c517bf0cc8bbb5
dova1 <allakov.dovlet@gmail.com>
2024-05-14 18:14:44 +0300

    updated

---
+++ src/test/java/com/phonebook/tests/CreateAccountTests.java
@@ -0,0 +1,29 @@
+package com.phonebook.tests;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CreateAccountTests extends TestBase{
+
+
+    @Test
+    public void createNewAccountPositiveTest(){
+        //click on Login link
+        driver.findElement(By.cssSelector("[href='/login']")).click();
+        //enter email
+        driver.findElement(By.name("email")).click();
+        driver.findElement(By.name("email")).clear();
+        driver.findElement(By.name("email")).sendKeys("altyn@gm.com");
+        //enter password
+        driver.findElement(By.name("password")).click();
+        driver.findElement(By.name("password")).clear();
+        driver.findElement(By.name("password")).sendKeys("Altyn1234!");
+        //click on Registration button
+        driver.findElement(By.name("registration")).click();
+        //assert Sign Out button is present
+        Assert.assertTrue(isElementPresent(By.cssSelector("button")));
+
+    }
+
+}
